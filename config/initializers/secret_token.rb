1d87fa1b09c90f6595daf55bfbe349fad8f4b64b
Daniel Nardi <daniel.b.nardi@gmail.com>
2014-02-18 13:51:29 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Labour::Application.config.secret_key_base = 'a7274012b23e6a3c9b75e982c41b5662faf37d6b617c616cf2ac0e8163ac2a464d60714b2febf160694a93a36caf191a40ec6e8e5aa481e14e8993e84416eb09'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Labour::Application.config.session_store :cookie_store, key: '_Labour_session'
